67bdd30692be224c214e70539a5d0e96b28e1066
Felipe Paz <xxfelipepaz@gmail.com>
2019-10-01 23:56:08 -0300

    Backup

--- app/src/components/MenuPrincipal.vue
+++ app/src/components/MenuPrincipal.vue
@@ -68,14 +68,7 @@ export default {
     return {
       drawer: this.$q.platform.is.desktop,
       menu: [
-        { label: 'Cadastros',
-          icon: 'mdi-account',
-          submenus: [
-            { label: 'Contas', icon: 'mdi-folder-alert', url: '' },
-            { label: 'Tipo de Conta', icon: 'mdi-folder-swap', url: '' },
-            { label: 'Utilizadores', icon: 'mdi-account-supervisor-circle', url: '' },
-            { label: 'Empresa', icon: 'mdi-office-building', url: '' }
-          ] },
+        { label: 'Cadastros', icon: 'mdi-account', submenus: [{ label: 'Contas', icon: 'mdi-folder-alert', url: '' }, { label: 'Tipo de Conta', icon: 'mdi-folder-swap', url: '' }, { label: 'Utilizadores', icon: 'mdi-account-supervisor-circle', url: '' }, { label: 'Empresa', icon: 'mdi-office-building', url: '' }] },
         { label: 'Financeiro', icon: 'mdi-cash-usd', submenus: [{ label: 'Tipo de Conta', icon: 'mdi-folder-alert', url: '' }, { label: 'Tipo de Documento', icon: 'mdi-file-alert', url: '' }, { label: 'Caixas/Bancos', icon: 'mdi-piggy-bank', url: '' }, { label: 'Contas Caixa/Banco', icon: 'mdi-bank-outline', url: '' }, { label: 'Modos de Pagamento', icon: 'mdi-cash-refund', url: '' }, { label: 'Tipo de Lançamento', icon: 'mdi-file-multiple', url: '' }, { label: 'Centros de Custo', icon: 'mdi-cash-marker', url: '' }, { label: 'Plano de Contas', icon: 'mdi-vector-ellipse', url: '', separator: true }, { label: 'Contas a pagar/receber', icon: 'mdi-cash-register', url: '' }, { label: 'Movimentação financeira', icon: 'mdi-folder-sync', url: '', separator: true }, { label: 'Detalhes das contas', icon: 'mdi-file-document-box-search', url: '' }, { label: 'Contas correntes', icon: 'mdi-format-rotate-90', url: '' }, { label: 'Importar', icon: 'mdi-cloud-upload', url: '', separator: true }, { label: 'Relatórios', icon: 'mdi-chart-areaspline', url: '' }] },
         { label: 'Passeios', icon: 'mdi-bike', submenus: [{ label: 'Atrativos', icon: 'mdi-star-face', url: '' }, { label: 'Atividades', icon: 'mdi-map-marker-question', url: '' }, { label: 'Agências', icon: 'mdi-briefcase-account', url: '' }, { label: 'Grupos', icon: 'mdi-group', url: '' }, { label: 'Guias', icon: 'mdi-location-enter', url: '' }, { label: 'Mensagens', icon: 'mdi-message-bulleted', url: '', separator: true }, { label: 'Reservas', icon: 'mdi-calendar-clock', url: '' }, { label: 'Histórico', icon: 'mdi-history', url: '' }, { label: 'Vouchers', icon: 'mdi-ticket-account', url: '', separator: true }, { label: 'Reservas recorrentes', icon: 'mdi-axis-y-rotate-clockwise', url: '' }, { label: 'Relatórios', icon: 'mdi-file-chart', url: '' }, { label: 'Gráficos', icon: 'mdi-chart-bar', url: '' }] },
         { label: 'Parceria Comercial', icon: 'mdi-hand', submenus: [{ label: 'Convites recebidos', icon: 'mdi-calendar-heart', url: '' }, { label: 'Enviar convite', icon: 'mdi-cube-send', url: '' }] },
--- app/src/pages/Error404.vue
+++ app/src/pages/Error404.vue
@@ -6,12 +6,12 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
+    <p class="text-faded">Ops, parece que o que você procurava não se encontra mais...<strong>(404)</strong></p>
     <q-btn
-      color="secondary"
+      color="purple-9"
       style="width:200px;"
-      to="/"
-      label="Go back"
+      :to="{name: 'PDashboard'}"
+      label="Voltar"
     />
   </div>
 </template>
